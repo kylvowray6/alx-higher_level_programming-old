f5015154129885722bad48ff81207bba6fc0026a
kylvowray6 <kylvowray6@gmail.com>
2023-10-02 12:42:53 -0700

    dkd

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print((str + '\n').split()[5] + " with " + (str + '\n').split()[-2])
+print(str[66:100] + str[100:106] + str[0:6])
+
