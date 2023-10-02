505c814accb1cfc00a875ce529e481a5cb2d3dd3
kylvowray6 <kylvowray6@gmail.com>
2023-10-02 12:52:52 -0700

    skd

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[66:100] + str[100:106] + str[0:6])
+print(str[66:81] + str[-22:-18] + str[1:2])
 
