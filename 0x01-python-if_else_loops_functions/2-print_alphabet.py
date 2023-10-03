d899970682274e1b7cdee8c84bdd491aa130d38f
kylvowray6 <kylvowray6@gmail.com>
2023-10-03 12:42:42 -0700

    skd

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'), ord('z') + 1):
-    print(chr(letter), end='')
+for c in range(97, 123):
+    print("{}".format(chr(c)), end="")
 
-print()  # Print a newline character to end the line
+print()
 
