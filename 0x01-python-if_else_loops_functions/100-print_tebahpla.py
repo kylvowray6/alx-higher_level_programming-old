bcb0d7511c7560a01f2e061b8f9b0dda52c698c5
kylvowray6 <kylvowray6@gmail.com>
2023-10-03 14:08:54 -0700

    ssk

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for char_code in range(ord('z'), ord('A') - 1, -1):
+    char = chr(char_code)
+
+    if char_code % 2 == 0:
+        print("{}".format(char), end='')
+
+print()
