9801c172f30a5f854490e0bcd8a09c130a12564a
kylvowray6 <kylvowray6@gmail.com>
2023-10-03 12:51:30 -0700

    sk

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    print("{}".format(chr(c)), end="")
+"""
+Prints the ASCII alphabet in lowercase, not followed by a new line.
+"""
+
+# Use a list comprehension to generate the characters
+alphabet = [chr(c) for c in range(97, 123)]
+
+# Use a single print function with string formatting to join the characters
+print("".join(alphabet), end="")
 
