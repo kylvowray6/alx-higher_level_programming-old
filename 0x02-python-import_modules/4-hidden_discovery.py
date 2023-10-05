03b52221d5774cf21c3217987534588f46116725
kylvowray6 <kylvowray6@gmail.com>
2023-10-05 11:48:40 -0700

    sks

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 if __name__ == "__main__":
     module_names = dir(hidden_4)
-    
+
+
     for name in sorted(module_names):
         if not name.startswith("__"):
             print(name)
