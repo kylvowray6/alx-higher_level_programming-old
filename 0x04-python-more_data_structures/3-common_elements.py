9883c32e716258a0b27928b5e9ab7e4587e63c43
kylvowray6 <kylvowray6@gmail.com>
2023-10-12 12:34:37 -0700

    kld

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+
 def common_elements(set_1, set_2):
     # Use set intersection to find the common elements between two sets
     common_set = set_1.intersection(set_2)
     return common_set
+
 
 # Example usage:
 if __name__ == "__main__":
