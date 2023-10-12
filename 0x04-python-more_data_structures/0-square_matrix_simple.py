8ad68bd8ec70cda96ec1001ae7013b1b5162dc8a
kylvowray6 <kylvowray6@gmail.com>
2023-10-12 12:21:48 -0700

    jd

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-   
-    # Create a new matrix with the same dimensions as the input matrix
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-           
-            # Calculate the square of each element and append it to the new row
-            new_row.append(element ** 2)
-        # Append the new row to the new_matrix
-        new_matrix.append(new_row)
-    
+    new_matrix = []  # Create an empty list to store the new matrix
 
-    return new_matrix
+    for row in matrix:  # Iterate through each row in the input matrix
+        new_row = []  # Create an empty list for the new row
+        for elem in row:  # Iterate through each element in the row
+            new_row.append(elem ** 2)  # Square the element and add it to the new row
+        new_matrix.append(new_row)  # Add the new row to the new matrix
+
+    return new_matrix  # Return the new matrix
+
+# Example usage:
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
