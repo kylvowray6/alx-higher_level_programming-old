68fb5025d87b856b497d6ae1822ab5ee8e94c36f
kylvowray6 <kylvowray6@gmail.com>
2023-10-12 12:33:19 -0700

    k

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
-
 def uniq_add(my_list=[]):
     unique_sum = 0  # Initialize a variable to store the sum of unique integers
 
-
     # Use a set to keep track of unique integers
     unique_set = set()
-
 
     for num in my_list:
         # Check if the number is not in the set (i.e., it's unique)
         if num not in unique_set:
             unique_set.add(num)  # Add the unique number to the set
             unique_sum += num  # Add the unique number to the sum
-
 
     return unique_sum
 
---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    # Use set intersection to find the common elements between two sets
+    common_set = set_1.intersection(set_2)
+    return common_set
+
+# Example usage:
+if __name__ == "__main__":
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    c_set = common_elements(set_1, set_2)
+    print(sorted(list(c_set)))
